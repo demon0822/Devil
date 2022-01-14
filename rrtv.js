18a881e147813905808389f0bc56deb005c2c07b
demon0822 <88918584+demon0822@users.noreply.github.com>
2022-01-14 20:19:21 +0800

    Create rrtv.js

---
+++ rrtv.js
@@ -0,0 +1,6 @@
+^https?:\/\/api\.rr\.tv\/watch\/v\d\/get_movie_info url script-response-body https://Choler.github.io/Surge/Script/rrsp.js
+^https?:\/\/api\.rr\.tv\/v\dplus\/index\/channel url script-response-body https://Choler.github.io/Surge/Script/rrsp.js
+^https:\/\/api\.rr\.tv\/user\/privilege\/list url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/rrtv.js
+^https:\/\/api\.rr\.tv\/ad\/getAll url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/rrtv.js
+^https:\/\/api\.rr\.tv\/drama\/app\/get_combined_drama_detail url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/rrtv.js
+hostname = api.rr.tv
